5080be63db3ecfb46455f08a67e9bc0b2b3da0dc
Samed <mustafasamedkasal@gmail.com>
2018-06-19 17:15:53 +0300

    prod source map option fixed

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -21,6 +21,7 @@ const publicPath = paths.servedPath;
 const shouldUseRelativeAssetPaths = publicPath === './';
 // Source maps are resource heavy and can cause out of memory issue for large source files.
 const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== 'false';
+
 // `publicUrl` is just like `publicPath`, but we will provide it to our app
 // as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
 // Omit trailing slash as %PUBLIC_URL%/xyz looks better than %PUBLIC_URL%xyz.
